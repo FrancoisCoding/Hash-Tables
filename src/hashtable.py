f901ab63864e5ee79ef9df9af732da8cf5d50411
isaiah francois <francoisisaiaht@yahoo.com>
2020-02-10 22:29:01 -0500

    Finished MVP

--- src/hashtable.py
+++ src/hashtable.py
@@ -20,7 +20,6 @@ class HashTable:
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return hash(key)
@@ -29,7 +28,6 @@ class HashTable:
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
@@ -46,45 +44,100 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        pass
+        #Overwrite keys when same key entered
+        if self.retrieve(key):
+            self.remove(key)
+        #Acquire index
+        index = self._hash_mod(key)
+        #Acqure the given Node
+        node = self.storage[index]
 
+        #If the node is empty at given index, add it and exit function
+        if node is None:
+            self.storage[index] = LinkedPair(key,value)
+            return
+        #Otherwise collision is dectected at given index
+        #Iterate through the LL
+        prev = node
+        while node is not None:
+            prev = node
+            node = node.next
+        #When the last node is found, add to it .next
+        prev.next = LinkedPair(key,value)
 
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        pass
-
+        #Acquire the index and node
+        index = self._hash_mod(key)
+        node = self.storage[index]
+        prev = None
+        while node is not None and node.key != key:
+            prev = node
+            node = node.next
+        #if nothing is found, return warning
+        if node is None:
+            return f"The given {key} is not in the hashtable"
+        else:
+            #The possible node has been found
+            removed = node.value
+            #Now delete the node appropriately
+            if prev is None:
+                #Either none or next possible match
+                self.storage[index] = node.next
+            else:
+                #Point the subsequent node and delete
+                prev.next = prev.next.next
+            return removed
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
-        pass
+        #Acquire the index
+        index = self._hash_mod(key)
+        #acquire the node at given index
+        node = self.storage[index]
 
+        #Loop through find the key or end of the list
+        while node is not None and node.key != key:
+            node = node.next
+        
+        #Once node is found, return if exists
+        if node is None:
+            return None
+        else:
+            return node.value
 
     def resize(self):
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
-        pass
+        #Create new Hashtable
+        ht = HashTable(self.capacity*2)
+
+        #Copy via loop
+        for node in self.storage:
+            current_node = node
+            while current_node is not None:
+                ht.insert(current_node.key, current_node.value)
+                current_node = current_node.next
+        
+        #Update existing Hashtable
+
+        self.capacity = self.capacity*2
+        self.storage = ht.storage
 
 
 
